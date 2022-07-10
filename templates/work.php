c92d14e49e49b284c85be76b501e421220b0e151
Nick McGinnis <nmcginnis17@users.noreply.github.com>
2022-07-10 09:40:10 -0400

    updated link lists

--- templates/work.php
+++ templates/work.php
@@ -3,19 +3,10 @@
 
     <div class="col-12 col-md-4">
       <div class="work-item">
-        <a href="http://www.sinnigmedia.com" target="_blank">
-          <div class="myWorkContent">
-            <img src="resources/logoMainWhite.png" alt="White Sinnig Media, LLC Logo" class="work-img">
-          </div>
-        </a>
-      </div>
-    </div>
-
-    <div class="col-12 col-md-4">
-      <div class="work-item">
         <a href="https://apps.apple.com/us/developer/sinnig-media-llc/id1541775279" target="_blank">
           <div class="myWorkContent">
-            <img src="resources/App_Store_Badge.svg" alt="White Sinnig Media, LLC Logo" class="work-img">
+            <!-- <img src="resources/App_Store_Badge.svg" alt="White Sinnig Media, LLC Logo" class="work-img"> -->
+            <h3 class="linkStyle">App Store</h3>
           </div>
         </a>
       </div>
@@ -25,7 +16,19 @@
       <div class="work-item">
         <a href="http://www.github.com/nmcginnis17" target="_blank">
           <div class="myWorkContent">
-            <img src="resources/Github_logo.png" alt="GitHub Logo" class="work-img">
+            <!-- <img src="resources/Github_logo.png" alt="GitHub Logo" class="work-img"> -->
+            <h3 class="linkStyle">GitHub</h3>
+          </div>
+        </a>
+      </div>
+    </div>
+
+    <div class="col-12 col-md-4">
+      <div class="work-item">
+        <a href="http://www.sinnigmedia.com" target="_blank">
+          <div class="myWorkContent">
+            <!-- <img src="resources/logoMainWhite.png" alt="White Sinnig Media, LLC Logo" class="work-img"> -->
+            <h3 class="linkStyle">Sinnig Media</h3>
           </div>
         </a>
       </div>
@@ -39,7 +42,8 @@
       <div class="work-item">
         <a id="js50link" href="http://www.nicholasmcginnis.com/50in50">
           <div class="myWorkContent">
-            <p id="js50label">JavaScipt 50</p>
+            <!-- <p id="js50label">JavaScipt 50</p> -->
+            <h3 class="linkStyle">JavaScript 50</h3>
           </div>
         </a>
       </div>
